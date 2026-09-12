25bae7515b4caa75b2f336453850e3180bce01bf
liakh10 <119670997+liakh10@users.noreply.github.com>
2026-09-13 01:41:21 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* ================= CONFIG: единственное место, где меняются данные ================= */
 window.SITE = {
-  CA:      "SOON",                       /* строго "SOON" до лаунча, потом 0x… (42 символа) */
+  CA:      "0x35eF87dD2e8A2193F1007e69cb8BA1CDFb420Ded",                       /* строго "SOON" до лаунча, потом 0x… (42 символа) */
   X_URL:   "https://x.com/neverpad_app",
   NAME:    "Never Pad",
   TICKER:  "$NEVERPAD",
